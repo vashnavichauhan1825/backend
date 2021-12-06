3ee81311726edad54db14a259211e8f60adec19b
vashnavichauhan1825 <70444778+vashnavichauhan1825@users.noreply.github.com>
2021-12-06 20:12:14 +0530

    create booklist

--- contact_list/index.js
+++ contact_list/index.js
@@ -7,10 +7,39 @@ var app = express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
+var bookList=[
+    {
+        name:"think like a monk",
+        author:'jay shetty'
+    },
+    {
+        name:'think and grow rich',
+        author:'napolean hill'
+    },
+    {
+        name:'the subtle art of not giving a f*ck',
+        author:'mark mansion'
+    },
+]
+
 app.get('/',function(req,res){
-    return res.render('main');
+    return res.render('main',{
+        title:"MY collection",
+        book_list:bookList
+});
 });
 
+app.get('/profile',function(req,res){
+    return res.render('profile',{
+        title:'colors all around !'
+      
+    })
+})
+
+
+app.post('/addBook', function(req,res){
+    return res.redirect('/profile');
+})
 app.listen(port,function(err){
     if(err){
         console.log(' there is an error', error);
