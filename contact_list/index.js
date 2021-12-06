888717d3ba987ef6b11fbd80d6edf1da2150fd87
vashnavichauhan1825 <70444778+vashnavichauhan1825@users.noreply.github.com>
2021-12-06 12:28:27 +0530

    send request

--- contact_list/index.js
+++ contact_list/index.js
@@ -3,6 +3,11 @@ const port=2118;
 
 var app = express();
 
+app.get('/about',function(req,res){
+    console.log(req.url);
+    res.send('<h1 style="background-color:grey">its working babes')
+});
+
 app.listen(port,function(err){
     if(err){
         console.log(' there is an error', error);
